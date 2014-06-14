892b3bad8605ec8d922614f800e5746a84fe7321
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-13 17:45:02 -0700

    Implement drawing enemies in an svg element

--- watchout.js
+++ watchout.js
@@ -1,2 +1,46 @@
 // start slingin' some d3 here.
 
+// create variables for height and width of eventual svg
+var height = 480;
+var width = 940;
+// select body and append svg element (canvas)
+  // set svg height and width
+var svg = d3.select('body').append('svg')
+          .attr('width', width)
+          .attr('height', height)
+        .append('g');
+// create data array
+var data = [];
+// iterate through range (how many enemies we want)
+for( var i = 0; i < 100; i++ ){
+  // push array item
+  data.push(i);
+}
+
+// function to generate random x coordinate
+var randomXCoord = function() {
+  return Math.floor(Math.random() * width);
+};
+
+// function to generate random y coordinate
+var randomYCoord = function() {
+  return Math.floor(Math.random() * height);
+};
+
+// create function for creating enemies - input data (array of indices)
+var createEnemies = function(data) {
+  // create variable to refer to selection of all circles in svg
+  svg.selectAll('circle')
+  // bind data to circles collection
+     .data(data)
+  // select empty nodes among circles, append circle svg to each node
+     .enter().append('circle')
+  // set coordinates per node
+     .attr('cx', randomXCoord)
+     .attr('cy', randomYCoord)
+     .attr('r', 4);
+  // (no return; result is side effect of data-bound circle nodes visible in canvas)
+};
+
+// call createEnemies function
+createEnemies(data);
